2a94949577dfcecbabaad159ce57a493205045e0
RAUL <00004017@uca.edu.sv>
2018-05-22 18:17:44 -0600

    nueva ventana :0

--- src/gui/Ventana.java
+++ src/gui/Ventana.java
@@ -4,11 +4,26 @@
  * and open the template in the editor.
  */
 package gui;
-
+import java.awt.Container;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.JTextField;
 /**
  *
  * @author Raul Calderon
  */
-public class Ventana {
-    
+public class Ventana extends JFrame {
+     public Ventana() {
+        super("Calculadora D:");
+        initComponent();
+    }
+
+    private void initComponent(){
+     setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLayout(null);   
+    }
+
 }
