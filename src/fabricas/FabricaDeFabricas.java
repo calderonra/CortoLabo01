8c6d03105c0a02b1834cde42c3c3c038a7ca6346
RAUL <00004017@uca.edu.sv>
2018-05-22 18:10:57 -0600

    nueva fabrica :0

---
+++ src/fabricas/FabricaDeFabricas.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fabricas;
+
+import aritmetico.aritmetico;
+
+/**
+ *
+ * @author Raul Calderon
+ */
+public class FabricaDeFabricas {
+   public static fabricaAbs getfabrica(String tipo){
+       if(tipo.equalsIgnoreCase("aritmetica")){
+           return new fabricaAritmetica();
+       }
+       return null;
+   } 
+}
