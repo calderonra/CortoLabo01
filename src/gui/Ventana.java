34bd558ea77f6db30d7d2c1e8b2bf615d8154c48
RAUL <00004017@uca.edu.sv>
2018-05-22 18:40:50 -0600

    Agregando cosas a la ventana :0

--- src/gui/Ventana.java
+++ src/gui/Ventana.java
@@ -16,6 +16,7 @@ import javax.swing.JTextField;
  * @author Raul Calderon
  */
 public class Ventana extends JFrame {
+    private JButton boton,boton2,boton3,boton4;
      public Ventana() {
         super("Calculadora D:");
         initComponent();
@@ -24,6 +25,28 @@ public class Ventana extends JFrame {
     private void initComponent(){
      setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setLayout(null);   
+         setSize(600, 300); //tamaño de la ventana 
+      Container contenedor = getContentPane();
+         boton = new JButton("Suma");
+        boton.setBounds(10,200, 150, 30);
+        boton2 = new JButton("resta");
+        boton2.setBounds(10,250, 150, 30);
+        boton3= new JButton("multiplicacion");
+        boton3.setBounds(10,300, 150, 30);
+        boton4 = new JButton("division");
+        boton4.setBounds(10,150, 150, 30);
+        contenedor.add(boton);
+        contenedor.add(boton2);
+        contenedor.add(boton3);
+        contenedor.add(boton4);
+        
     }
-
+public static void main(String[] args) {
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                new Ventana().setVisible(true);
+            }
+        });
+    }
 }
