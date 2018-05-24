ce7ac9df040c664b512260a52322b270564260e5
RAUL <00004017@uca.edu.sv>
2018-05-23 20:33:28 -0600

    Configurando la ventana

--- src/gui/Ventana.java
+++ src/gui/Ventana.java
@@ -21,7 +21,7 @@ import javax.swing.JTextField;
  */
 public class Ventana extends JFrame {
     private JButton boton,boton2,boton3,boton4,boton5;
-    private JTextField caja1,caja2,caja3;
+    private JTextField caja1,caja2,caja3,caja4;
     fabricaAbs fabrica;
      public Ventana() {
         super("Calculadora D:");
@@ -31,7 +31,7 @@ public class Ventana extends JFrame {
     private void initComponent(){
      setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setLayout(null);   
-         setSize(600, 600); //tamaño de la ventana 
+         setSize(500, 500); //tamaño de la ventana 
       Container contenedor = getContentPane();
          boton = new JButton("Suma");
         boton.setBounds(10,200, 150, 30);
@@ -49,15 +49,19 @@ public class Ventana extends JFrame {
         contenedor.add(boton3);
         contenedor.add(boton4);
         contenedor.add(boton5);
-        caja1 = new JTextField();
-        caja1.setBounds(300, 200, 150, 30);
+        
+        caja1 = new JTextField("numero a operar");
+        caja1.setBounds(300, 150, 150, 30);
         contenedor.add(caja1);
-        caja2 = new JTextField();
-        caja2.setBounds(300, 250, 150, 30);
+        caja2 = new JTextField("numero a operar 2");
+        caja2.setBounds(300, 200, 150, 30);
         contenedor.add(caja2);
-        caja3 = new JTextField();
-        caja3.setBounds(300, 350, 150, 30);
+        caja3 = new JTextField("total");
+        caja3.setBounds(300, 300, 150, 30);
         contenedor.add(caja3);
+        caja4 = new JTextField("Introduce el numero a convertir");
+        caja4.setBounds(300, 50, 150, 30);
+        contenedor.add(caja4);
         boton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -129,7 +133,7 @@ public class Ventana extends JFrame {
                 String aux1,aux2,saltin;
                 fabrica = FabricaDeFabricas.getfabrica("conversor");
                 conversor bin=fabrica.getconversor("conversor");
-                aux1=caja1.getText();
+                aux1=caja4.getText();
                 num1=Integer.parseInt(aux1);
                 calculo=bin.convertir(num1);
                 saltin=String.valueOf(calculo);
